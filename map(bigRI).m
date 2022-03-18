b205a9aeb9f8f85102bf90e66fe19a84ed6b9597
maldana122 <101124134+maldana122@users.noreply.github.com>
2022-03-17 21:32:05 -0400

    Add files via upload
    Attempt to create large map

---
+++ map(bigRI).m
@@ -0,0 +1,19 @@
+states=shaperead('usastatehi.shp', 'UseGeoCoords', true);
+ri=states(strcmp({states.Name}, 'Rhode Island'));
+latlim=[min(ri.Lat) max(ri.Lat)];
+lonlim=[min(ri.Lon) max(ri.Lon)];
+
+figure
+h1=usamap(latlim, lonlim)
+setm(h1,'FFaceColor','#B7E9F7')
+geoshow(states, 'FaceColor','#EFE6DC')
+geoshow(ri, 'FaceColor','#90EE90')
+
+h2 = axes('Position',[0.15 0.25 0.2 0.2]);
+usamap({'PA', 'ME'})
+setm(h2,'FFaceColor','w')
+plabel off
+mlabel off
+gridm off
+geoshow(states, 'FaceColor','#EDEDED')
+geoshow(ri, 'FaceColor', '#90EE90')
