acb31a1b47b4bff56863ed28d6c2e498e90982c5
maldana122 <101124134+maldana122@users.noreply.github.com>
2022-03-21 10:05:02 -0400

    finalized location code- fully working
    location code with drop-down options

--- locations.m
+++ locations.m
@@ -8,63 +8,36 @@ disp("You chose" + opts(choice))
 %Nelson
 location_choices = containers.Map;
 location_choices('Nelson Fitness Center') = [41.83,-71.3980];
+location_choices('The Ratty')= [41.8252, -71.4012]; 
+location_choices('The Sci-Li')= [41.82720, -71.4002];
+location_choices('The Rock')= [41.8257, -71.4051];
+location_choices('Andrews Dining Hall')= [41.8306, -71.4024];
 
-temp5 = location_choices(opts(choice));
-latchoice = temp5(1) 
-lonchoice = temp5(2)
+temp = location_choices(opts(choice));
+latchoice = temp(1) 
+lonchoice = temp(2)
 
+%Nelson
 latNelson=41.83;
 lonNelson=-71.3980;
 
 
 %Ratty
-
-location_choices=containers.Map;
-location_choices('The Ratty')= [41.8252, -71.4012];
-
-temp1 = location_choices(opts(choice));
-latchoice = temp1(1) 
-lonchoice = temp1(2)
-
 latRatty=41.8252;
 lonRatty=-71.4012;
 
 
 %Sci-Li
-
-location_choices=containers.Map;
-location_choices('The Sci-Li')= [41.82720, -71.4002];
-
-temp2 = location_choices(opts(choice));
-latchoice = temp2(1) 
-lonchoice = temp2(2)
-
 latScili=41.8270;
 lonScili=-71.4002;
 
 
 %The Rock
-
-location_choices=containers.Map;
-location_choices('The Rock')= [41.8257, -71.4051];
-
-temp3 = location_choices(opts(choice));
-latchoice = temp3(1) 
-lonchoice = temp3(2)
-
 latRock=41.8257;
 lonRock=-71.4051;
 
 
 %Andrews
-
-location_choices=containers.Map;
-location_choices('Andrews Dining Hall')= [41.8306, -71.4024];
-
-temp4 = location_choices(opts(choice));
-latchoice = temp4(1) ;
-lonchoice = temp4(2);
-
 latAndrews=41.8306;
 lonAndrews=-71.4024;
 
@@ -76,124 +49,61 @@ disp("You chose" + opts(choice))
 
 %Ice-cream places
 
-%strawberry ice-cream
-
 destination_choices = containers.Map;
 destination_choices('Strawberry Ice Cream') = [41.8187,-71.4247];
+destination_choices('Chocolate Ice Cream') = [41.8243,-71.4249];
+destination_choices('Vanilla Ice Cream') = [41.8229,-71.4008];
+destination_choices('Mint Chocolate Chip') = [41.8229,-71.3924];
+destination_choices('Chocolate Chip Cookie Dough') = [41.8214,-71.3922];
+destination_choices('Jamoca') = [41.8304,-71.4008];
+destination_choices('Rocky Road') = [41.8292,-71.4168];
+destination_choices('Chocolate Chip') = [41.8292,-71.4168];
+destination_choices('Rainbow Sherbert') = [41.8187,-71.4247];
+destination_choices('Pralines') = [41.82303,-71.37914];
 
-temp6 = destination_choices(opts(choice));
-latend = temp6(1); 
-lonend = temp6(2);
+temp2 = destination_choices(opts(choice));
+latend = temp2(1); 
+lonend = temp2(2);
 
+%strawberry ice-cream
 latTri=41.8187;
 lonTri=-71.4247;
 
 %chocolate ice cream
-
-destination_choices = containers.Map;
-destination_choices('Chocolate Ice Cream') = [41.8243,-71.4249];
-
-temp7 = destination_choices(opts(choice));
-latend = temp7(1); 
-lonend = temp7(2);
-
 latHelado=41.8243;
 lonHelado=-71.4249;
 
 %Vanilla ice cream
-
-destination_choices = containers.Map;
-destination_choices('Vanilla Ice Cream') = [41.8229,-71.4008];
-
-temp8 = destination_choices(opts(choice));
-latend = temp8(1); 
-lonend = temp8(2);
-
 latBenJ=41.8229;
 lonBenJ=-71.4008;
 
 %mint cholocate chip
-destination_choices = containers.Map;
-destination_choices('Mint Chocolate Chip') = [41.8229,-71.3924];
-
-temp9 = destination_choices(opts(choice));
-latend = temp9(1); 
-lonend = temp9(2);
-
 latUdder=41.8229;
 lonUdder=-71.3924;
 
 %chocolate chip cookie dough
-
-destination_choices = containers.Map;
-destination_choices('Chocolate Chip Cookie Dough') = [41.8214,-71.3922];
-
-temp10 = destination_choices(opts(choice));
-latend = temp10(1) 
-lonend = temp10(2);
-
 latKow=41.8214;
 lonKow=-71.3922;
 
 %jamoca 
-
-destination_choices = containers.Map;
-destination_choices('Jamoca') = [41.8304,-71.4008];
-
-temp11 = destination_choices(opts(choice));
-latend = temp11(1); 
-lonend = temp11(2);
-
 latInsomnia=41.8304;
 lonInsomnia=-71.4008;
 
 
 %Rocky Road
-
-destination_choices = containers.Map;
-destination_choices('Rocky Road') = [41.8292,-71.4168];
-
-temp13 = destination_choices(opts(choice));
-latend = temp13(1); 
-lonend = temp13(2);
-
 latDairyQ=41.8292;
 lonDairyQ=-71.4168;
 
 
 %Chocolate Chip
-
-destination_choices = containers.Map;
-destination_choices('Chocolate Chip') = [41.8292,-71.4168];
-
-temp15 = destination_choices(opts(choice));
-latend = temp15(1); 
-lonend = temp15(2);
-
 latDairyQ=41.8292;
 lonDairyQ=-71.4168;
 
 %Rainbow Sherbet
-
-destination_choices = containers.Map;
-destination_choices('Rainbow Sherbert') = [41.8187,-71.4247];
-
-temp14 = destination_choices(opts(choice));
-latend = temp14(1); 
-lonend = temp14(2);
-
 latTri=41.8187;
 lonTri=-71.4247;
 
 %Pralines
-
-destination_choices = containers.Map;
-destination_choices('Pralines') = [41.82303,-71.37914];
-
-temp12 = destination_choices(opts(choice));
-latend = temp12(1); 
-lonend = temp12(2);
-
 latSacred=41.82303;
 lonSacred=-71.37914;
 
